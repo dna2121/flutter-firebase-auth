6822e624a8a5d48b80a9aae8d3e3603e68fb945b
dna2121 <dnatayawibisono@gmail.com>
2023-11-17 16:49:55 +0700

    signup ui

--- lib/auth/views/signup_page.dart
+++ lib/auth/views/signup_page.dart
@@ -6,8 +6,71 @@ class SignupPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Sign Up"),
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset('assets/images/signup.png', height: 300),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("Sign up",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.w700)),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "email",
+                    prefixIcon: Icon(Icons.alternate_email),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "password",
+                    prefixIcon: Icon(Icons.lock_outline),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "name",
+                    prefixIcon: Icon(Icons.badge_outlined),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.amber,
+                      minimumSize: Size(MediaQuery.of(context).size.width * 1,
+                          MediaQuery.of(context).size.height * 0.06),
+                    ),
+                    child: const Text(
+                      "Sign up",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  )),
+              const SizedBox(height: 10),
+            ],
+          ),
+        ),
       ),
     );
   }
