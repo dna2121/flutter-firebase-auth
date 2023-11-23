a01a7d858b19ed6b975eb5597819672ec9e4950e
dna2121 <dnatayawibisono@gmail.com>
2023-11-23 23:21:54 +0700

    create auth controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_authentication/app_pages.dart';
+import 'package:flutter_firebase_authentication/auth/controller/auth_controller.dart';
 import 'package:flutter_firebase_authentication/auth/views/signin_page.dart';
 import 'package:get/get.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      options: const FirebaseOptions(
-          apiKey: "AIzaSyCa3c9Vsq2ECW4fWbXXFAKmOzkJJxWaAPI",
-          appId: "1:1073308277702:ios:1b9501f81c270564b1f0c7",
-          messagingSenderId: "1073308277702",
-          projectId: "auth-d10c6"));
-  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+    options: const FirebaseOptions(
+        apiKey: "AIzaSyCa3c9Vsq2ECW4fWbXXFAKmOzkJJxWaAPI",
+        appId: "1:1073308277702:ios:1b9501f81c270564b1f0c7",
+        messagingSenderId: "1073308277702",
+        projectId: "auth-d10c6"),
+  ).then((value) => Get.put(AuthController()));
   runApp(const MyApp());
 }
 
